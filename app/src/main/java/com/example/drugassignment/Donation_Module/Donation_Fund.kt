110336ea63971149e4a7e69ceef1ae6248674d83
Kuek Yong Boon <kuekyb@gmail.com>
2020-01-06 06:41:08 +0800

    finish linking

--- app/src/main/java/com/example/drugassignment/Donation_Module/Donation_Fund.kt
+++ app/src/main/java/com/example/drugassignment/Donation_Module/Donation_Fund.kt
@@ -6,8 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 
 import com.example.drugassignment.R
+import com.example.drugassignment.databinding.FragmentDonationFundBinding
+import com.example.drugassignment.databinding.FragmentDonationMainBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -19,7 +23,17 @@ class Donation_Fund : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_donation__fund, container, false)
+        val binding : FragmentDonationFundBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_donation__fund, container, false
+        )
+
+        val navController = activity!!.findNavController(R.id.nav_host_fragment)
+
+        binding.buttonNext.setOnClickListener {
+            navController.navigate(R.id.action_donation_Fund_to_donation_Fund_Payment)
+        }
+
+        return binding.root
     }
 
 
