6e3191fb3da2e9f291e56d4fd22ee9dd68b6d888
E Jet <52629564+seeej-wp17@users.noreply.github.com>
2020-01-06 03:18:18 +0800

    Created All UI

--- app/src/main/java/com/example/drugassignment/Donation_Module/Donation_Main.kt
+++ app/src/main/java/com/example/drugassignment/Donation_Module/Donation_Main.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.drugassignment.R
+import kotlinx.android.synthetic.main.fragment_donation__main.*
 
 /**
  * A simple [Fragment] subclass.
