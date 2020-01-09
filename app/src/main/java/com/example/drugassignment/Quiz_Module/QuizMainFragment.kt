c046fac6f476c8b504cb44747e60b09f5c29bb01
joeyelim <43492692+joeyelim@users.noreply.github.com>
2020-01-10 00:33:03 +0800

    newUpdate

--- app/src/main/java/com/example/drugassignment/Quiz_Module/QuizMainFragment.kt
+++ app/src/main/java/com/example/drugassignment/Quiz_Module/QuizMainFragment.kt
@@ -2,24 +2,34 @@ package com.example.drugassignment.Quiz_Module
 
 
 import android.os.Bundle
+import android.view.*
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.drugassignment.R
+import com.example.drugassignment.databinding.FragmentQuizMainBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class QuizMainFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_quiz_main, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding = DataBindingUtil.inflate<FragmentQuizMainBinding>(inflater,
+            R.layout.fragment_quiz_main, container, false)
+
+        binding.playButton.setOnClickListener { view : View ->
+            view.findNavController()
+                .navigate(QuizMainFragmentDirections.actionQuizMainToQuizQuesFragment())
+        }
+
+        setHasOptionsMenu(true)
+
+        return binding.root
     }
+
 
 
 }
