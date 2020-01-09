432e45272d55ebc67cf84ce67a7bec945823a020
E Jet <52629564+seeej-wp17@users.noreply.github.com>
2020-01-10 01:54:45 +0800

    overwrite donation_venue_information

--- app/src/main/java/com/example/drugassignment/Donation_Module/Donation_Fund.kt
+++ app/src/main/java/com/example/drugassignment/Donation_Module/Donation_Fund.kt
@@ -2,16 +2,19 @@ package com.example.drugassignment.Donation_Module
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
-
 import com.example.drugassignment.R
 import com.example.drugassignment.databinding.FragmentDonationFundBinding
-import com.example.drugassignment.databinding.FragmentDonationMainBinding
+import kotlinx.android.synthetic.main.fragment_donation__fund.*
+
 
 /**
  * A simple [Fragment] subclass.
@@ -26,6 +29,14 @@ class Donation_Fund : Fragment() {
         val binding : FragmentDonationFundBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_donation__fund, container, false
         )
+        val textViewSpecifyAmount = view?.findViewById<TextView>(R.id.textViewSpecifyAmount)
+        val buttonOtherAmount = view?.findViewById<Button>(R.id.btnOtherAmount)
+        val editAmount = view?.findViewById<TextView>(R.id.editAmount)
+        btnOtherAmount.setOnClickListener{
+            textViewSpecifyAmount?.visibility = View.VISIBLE
+            editAmount?.visibility = View.VISIBLE
+        }
+
 
         val navController = activity!!.findNavController(R.id.nav_host_fragment)
 
@@ -35,6 +46,8 @@ class Donation_Fund : Fragment() {
 
         return binding.root
     }
+
+
 
 
 }
